f9cf1b74fdf8c244d7221cda531da92bb2b67168
Maksatbek Bolushov <maksatbek.bolushov@gmail.com>
2021-12-15 14:52:54 +0600

    Fix pipeline unit test

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -1,17 +1,10 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Pipeline from '../lib/pipeline-stack';
+import { SynthUtils } from '@aws-cdk/assert';
+import { App } from '@aws-cdk/core';
+import { PipelineStack } from '../lib/pipeline-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/pipeline-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Pipeline.PipelineStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('Pipeline Stack', () => {
+  const app = new App();
+  const stack = new PipelineStack(app, 'PipelineStack');
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
 });
